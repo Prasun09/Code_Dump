8e705baa30b775b93b754ba208b7d0de914354ab
Prasun <132467019+Prasun09@users.noreply.github.com>
2023-10-01 01:32:53 +0530

    Update OddOrEven.java
    Done....

--- java/OddOrEven.java
+++ java/OddOrEven.java
@@ -1,7 +1,17 @@
-Class OddOrEven
+//using package so that user can give input
+import java.util.Scanner;
+class OddOrEven
 {
-    public Static void main(String arg[])
+    public static void main(String arg[])
     {
-        
+    //promote user for input
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        {
+        System.out.println(n +" Is Even");
+        }else{
+        System.out.println(n+" Is Odd");
+        }
     }
 }
